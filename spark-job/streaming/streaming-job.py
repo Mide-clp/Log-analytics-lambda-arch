c96f81bf7a1c4e6f04a8e233425ca129a99dd2e0
Mide clp <aogunnola@gmail.com>
2022-09-21 03:45:09 +0100

    parsed log files into columns

--- spark-job/streaming/streaming-job.py
+++ spark-job/streaming/streaming-job.py
@@ -1,7 +1,30 @@
-from pyspark.sql import SparkSession
 import findspark
+from pyspark.sql import SparkSession
+from pyspark.sql import functions as func
 
 TOPIC = "log_flow"
+
+
+def format_logData(raw_df):
+    host_exp = r"(^\d{1,3}.\d{1,3}.\d{1,3})"
+    time_stamp_exp = r"(\d{1,3}\/\w{1,3}\/\w{1,4}:\d{1,2}:\d{1,2}:\d{1,2} \+\d{1,4})"
+    genera_exp = r'\"(\S+)\s(\S+)\s(\S+)"'
+    status = r"\s(\d{3})\s"
+    content_exp = r'\s(\d+) "'
+    useragent_referer_exp = r'"([\S ]*)" [0-9]+ [0-9]+ "([\S ]*)" "([\S |-]*)" '
+
+    data = raw_df.select(func.regexp_extract("value", host_exp, 1).alias("host"),
+                         func.regexp_extract("value", time_stamp_exp, 1).alias("timestamp"),
+                         func.regexp_extract("value", genera_exp, 1).alias("method"),
+                         func.regexp_extract("value", genera_exp, 2).alias("endpoint"),
+                         func.regexp_extract("value", genera_exp, 3).alias("protocol"),
+                         func.regexp_extract("value", status, 1).cast("int").alias("status"),
+                         func.regexp_extract("value", content_exp, 1).cast("int").alias("content_size"),
+                         func.regexp_extract("value", useragent_referer_exp, 2).alias("referer"),
+                         func.regexp_extract("value", useragent_referer_exp, 3).alias("useragent"))
+
+    return data
+
 
 if __name__ == "__main__":
     spark = SparkSession.builder.master("local[*]").appName("log-analytics").getOrCreate()
@@ -10,13 +33,15 @@ if __name__ == "__main__":
         .format("kafka") \
         .option("kafka.bootstrap.servers", "localhost:9092") \
         .option("subscribe", TOPIC) \
+        .option("checkpointLocation", "checkpoint") \
         .option("startingOffsets", "latest") \
         .load()
 
-    df.writeStream \
+    parsed_df = format_logData(df)
+
+    parsed_df.writeStream \
         .format("console") \
         .outputMode("append") \
         .start() \
         .awaitTermination()
     spark.stop()
-
