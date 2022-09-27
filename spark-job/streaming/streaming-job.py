19c975151a623bce788ece838bfd32b72c59b0f5
Mide clp <aogunnola@gmail.com>
2022-09-27 15:19:45 +0100

    add streaming to cassandra

--- spark-job/streaming/streaming-job.py
+++ spark-job/streaming/streaming-job.py
@@ -28,6 +28,19 @@ def format_logData(raw_df):
 
 
 def parse_date_and_time(data):
+    data = data.withColumn("timestamp", func.regexp_replace("timestamp", r"Jan", "1"))
+    data = data.withColumn("timestamp", func.regexp_replace("timestamp", r"Feb", "2"))
+    data = data.withColumn("timestamp", func.regexp_replace("timestamp", r"Mar", "3"))
+    data = data.withColumn("timestamp", func.regexp_replace("timestamp", r"Apr", "4"))
+    data = data.withColumn("timestamp", func.regexp_replace("timestamp", r"May", "5"))
+    data = data.withColumn("timestamp", func.regexp_replace("timestamp", r"Jun", "6"))
+    data = data.withColumn("timestamp", func.regexp_replace("timestamp", r"Jul", "7"))
+    data = data.withColumn("timestamp", func.regexp_replace("timestamp", r"Aug", "8"))
+    data = data.withColumn("timestamp", func.regexp_replace("timestamp", r"Sep", "9"))
+    data = data.withColumn("timestamp", func.regexp_replace("timestamp", r"Oct", "10"))
+    data = data.withColumn("timestamp", func.regexp_replace("timestamp", r"Nov", "11"))
+    data = data.withColumn("timestamp", func.regexp_replace("timestamp", r"Dec", "12"))
+
     d2 = data.withColumn("timestamp", parse_datetime(func.col("timestamp")).cast("timestamp"))
     d2 = d2.withColumn("hour", func.hour(func.col("timestamp")))
     d2 = d2.withColumn("day", func.dayofmonth(func.col("timestamp")))
@@ -52,13 +65,16 @@ def select_crawlers(data):
                                                                 1))
     comp_crawler_df = crawled_df.withColumn("crawler", func.when(
         func.col("crawler") == "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)",
-        "Googlebot Desktop").when(func.col(
-        "crawler") == "Mozilla/5.0 AppleWebKit/537.36 (KHTML, like Gecko; compatible; Googlebot/2.1; "
-                      "+http://www.google.com/bot.html)", "Googlebot Desktop").when(
-        func.col("crawler") == "Safari/537.36 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)",
-        "Googlebot Smartphone").when(
-        func.col("crawler") == "Mozilla/5.0 (compatible; bingbot/2.0; +http://www.bing.com/bingbot.htm)",
-        "Bingbot Desktop"))
+        "Googlebot Desktop").when(
+        func.col(
+            "crawler") == "Mozilla/5.0 AppleWebKit/537.36 (KHTML, like Gecko; compatible; "
+                          "Googlebot/2.1; +http://www.google.com/bot.html)", "Googlebot Desktop")
+                                            .when(
+        func.col("crawler") == "Safari/537.36 (compatible; Googlebot/2.1; "
+                               "+http://www.google.com/bot.html)", "Googlebot Smartphone")
+                                            .when(
+        func.col("crawler") == "Mozilla/5.0 (compatible; bingbot/2.0; "
+                               "+http://www.bing.com/bingbot.htm)", "Bingbot Desktop"))
 
     comp_crawler_df = comp_crawler_df.where(func.col("crawler") != "")
 
@@ -91,43 +107,61 @@ def parse_datetime(s):
     return str(new_date)
 
 
+def write_to_cassandra(comp_df, batch_id):
+    comp_df.write\
+        .format("org.apache.spark.sql.cassandra") \
+        .option("spark.cassandra.connection.host", "127.0.0.1") \
+        .option("spark.cassandra.connection.port", "9042") \
+        .option("keyspace", "log_analytics") \
+        .option("table", "status_code") \
+        .mode("append") \
+        .save()
+
+
+# .option("confirm.truncate", "true")
+
 if __name__ == "__main__":
     spark = SparkSession.builder.master("local[*]").config("dfs.client.use.datanode.hostname", "true").appName(
         "log-analytics").getOrCreate()
 
     df = spark.readStream \
         .format("kafka") \
-        .option("kafka.bootstrap.servers", "kafka:29092") \
+        .option("kafka.bootstrap.servers", "localhost:9092") \
         .option("subscribe", TOPIC) \
         .option("startingOffsets", "latest") \
         .load()
 
-    parsed_df = df(df)
+    # df.writeStream.outputMode()
 
-    df = parsed_df.withColumn("timestamp", func.regexp_replace("timestamp", r"Jan", "1").alias("timestamp"))
+    parsed_df = format_logData(df)
 
-    parse_df_datetime = parse_date_and_time(df)
+    parse_df_datetime = parse_date_and_time(parsed_df)
 
-    crawled_df = select_crawlers(parse_df_datetime)
+    crawler_df = select_crawlers(parse_df_datetime)
 
-    parsed_df.writeStream \
-        .partitionBy("timestamp") \
+    daily_status = crawler_df.select(func.col("status"), func.col("hour"), func.col("crawler"),
+                                     func.col("date")).groupby("status", "hour", "date", "crawler") \
+        .agg(func.count("status").alias("count"))
+
+    crawler_df.writeStream \
+        .partitionBy("day", "month", "year") \
         .format("parquet") \
         .option("checkpointLocation", "checkpoint") \
         .outputMode("append") \
         .start(path="hdfs://namenode:8020/data/")
 
-    parsed_df.writeStream \
-        .option("checkpointLocation", "checkpoint") \
-        .format("console") \
-        .outputMode("append") \
-        .start()
-
-    parsed_df.awaitTermination()
+    daily_status.writeStream \
+        .option("checkpointLocation", "tmp/checkpoint") \
+        .foreachBatch(write_to_cassandra) \
+        .outputMode("update") \
+        .start() \
+        .awaitTermination()
+    # "confirm.truncate"
+    # crawler_df.awaitTermination()
     spark.stop()
 
-# spark-submit --packages org.apache.spark:spark-streaming-kafka-0-10_2.12:3.2.1,org.apache.spark:spark-sql-kafka-0-10_2.12:3.2.1 ./spark-job/streaming/streaming-job.py
+# spark-submit --packages org.apache.spark:spark-streaming-kafka-0-10_2.12:3.2.1,org.apache.spark:spark-sql-kafka-0-10_2.12:3.2.1,com.datastax.spark:spark-cassandra-connector_2.12:3.0.0 ./spark-job/streaming/streaming-job.py
 
-# docker exec spark-master /spark/bin/spark-submit --master spark://localhost:7077 --packages org.apache.spark:spark-streaming-kafka-0-10_2.12:3.2.1,org.apache.spark:spark-sql-kafka-0-10_2.12:3.2.1 opt/spark_store/streaming/streaming-job.py
+# docker exec spark-master /spark/bin/spark-submit --master spark://localhost:7077 --packages org.apache.spark:spark-streaming-kafka-0-10_2.12:3.2.1,org.apache.spark:spark-sql-kafka-0-10_2.12:3.2.1,com.datastax.spark:spark-cassandra-connector_2.12:3.0.0 opt/spark_store/streaming/streaming-job.py
 
 # docker exec spark-master /spark/bin/spark-submit  --master spark://localhost:7077 opt/spark_store/batch/batch-job.py
