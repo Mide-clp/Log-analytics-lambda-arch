579824ade21bd947bb6d41dbdfafd3d23c6390c6
Mide clp <aogunnola@gmail.com>
2022-09-30 01:31:48 +0100

    add file type chart

--- visualization_dash/main.py
+++ visualization_dash/main.py
@@ -1,4 +1,6 @@
-from dash import Dash, html, Input, Output, dcc
+import datetime
+
+from dash import Dash, html, Input, Output, dcc, dash_table
 import json
 import plotly.express as px
 import plotly.graph_objects as go
@@ -9,6 +11,8 @@ from read_cassandra import get_cassandra_data
 app = Dash(__name__)
 
 sql = "SELECT * FROM status_code"
+
+crawler_columns = ["Top directory", "Total Bot Hits"]
 
 data = get_cassandra_data(sql)
 
@@ -56,7 +60,7 @@ app.layout = \
                             "top": "250px", "left": "210px"})
         ]),
         html.Div([
-            html.Div([dcc.Graph(id="status_code_bar")], style={"backgroundColor": "#C4C4C4", "position": "absolute",
+            html.Div([], style={"backgroundColor": "#C4C4C4", "position": "absolute",
                                                                "height": "522px", "width": "55%",
                                                                "top": "299px", "left": "210px",
                                                                "boxShadow": "0px 1px 4px rgba(0, 0, 0, 0.25)"}),
@@ -72,7 +76,36 @@ app.layout = \
 
         ], ),
 
-        dcc.Store(id="store_data")
+        dcc.Store(id="store_data"),
+        html.Div([
+            html.Div(
+                [dcc.Graph(id="status_code_bar")], style={"backgroundColor": "#C4C4C4", "position": "absolute",
+                           "height": "522px", "width": "55%",
+                           "top": "859px", "left": "210px",
+                           "boxShadow": "0px 1px 4px rgba(0, 0, 0, 0.25)"}
+            ),
+
+            html.Div(
+                [], style={"backgroundColor": "#C4C4C4", "position": "absolute",
+                           "height": "522px", "width": "55%",
+                           "top": "1420px", "left": "210px",
+                           "boxShadow": "0px 1px 4px rgba(0, 0, 0, 0.25)"}
+            ),
+
+            html.Div(
+                [html.Table(dash_table.DataTable(
+                    id="crawler_table",
+                    columns=[{"name": name, "id": name} for name in crawler_columns],
+                    style_data=dict(backgroundColor="#FFFFF"),
+                    style_table=dict(top="0px", width="248%", line_color="#FFA63E", textAlign="center",
+                                     boxShadow="0px 0px 0px rgba(0, 0, 0, 0.25)"),
+                    style_header=dict(color="white", backgroundColor="#FFA63E", fontWeight="bold", textAlign="center")
+                ))], style={"backgroundColor": "#FFFFF", "position": "absolute",
+                            "height": "600px", "width": "22.9%",
+                            "top": "859px", "left": "1575px",
+                            "boxShadow": "0px 1px 4px rgba(0, 0, 0, 0.25)"}
+            )
+        ])
     ],
 
         # style={"backgroundColor": "#7FDBFF"}
@@ -113,16 +146,29 @@ def update_date(date_input):
     Input('select_date', 'value')
 )
 def get_data(bot, date):
-    sql_stmt = "SELECT * FROM status_code"
+    sql_stmt = "SELECT * FROM status_code;"
+    crawler_freq_stmt = "SELECT * FROM crawler_frequency;"
+    file_type_stmt = "SELECT * FROM file_type;"
+
+    crawler_freq_data = get_cassandra_data(crawler_freq_stmt)
+    file_type_data = get_cassandra_data(file_type_stmt)
+    # print(file_type_data)
     c_data = get_cassandra_data(sql_stmt)
 
+    c_data["status"] = c_data["status"].astype("str")
+    crawler_df = crawler_freq_data[crawler_freq_data["crawler"] == bot]
     status_df = c_data[c_data["crawler"] == bot]
+    file_type_df = file_type_data[file_type_data["crawler"] == bot]
 
     if bot == "All Bots":
         status_df = c_data
+        crawler_df = crawler_freq_data
+        file_type_df = file_type_data
 
     status_df["date"] = pd.to_datetime(status_df["date"])
-
+    crawler_df["date"] = pd.to_datetime(crawler_df["date"])
+    file_type_df["date"] = pd.to_datetime(file_type_df["date"])
+    print(file_type_df)
     update_max = max(status_df["date"])
     update_min = min(status_df["date"])
 
@@ -131,21 +177,61 @@ def get_data(bot, date):
     # comp_df = ""
 
     status_df_pie = ""
+    crawler_df_gran = ""
+    file_type_df_pie = ""
+
+    # specify the period of data needed, and get the total frequency for that period
     if date == "Today":
         status_df_pie = status_df[status_df["date"] == pd.to_datetime(d_max)]
+        status_df_pie = status_df_pie.groupby(["status", "hour"]).agg({"frequency": "sum"}).reset_index()
+
+        file_type_df_pie = file_type_df[file_type_df["date"] == pd.to_datetime(d_max)]
+        print(file_type_df)
+        file_type_df_pie = file_type_df_pie.groupby(["file_type", "hour"]).agg({"frequency": "sum"}).reset_index()
+
+        crawler_df_gran = crawler_df[crawler_df["date"] == pd.to_datetime(d_max)]
+        crawler_df_gran = crawler_df_gran.groupby(["top_directory"]).agg({"frequency": "sum"}).reset_index()
 
     elif date == "30days":
         status_df_pie = status_df[
             (status_df["date"] >= pd.to_datetime(last_30days)) & (status_df["date"] <= pd.to_datetime(d_max))]
+        status_df_pie["date"] = pd.to_datetime(status_df_pie["date"]).dt.tz_localize(None).dt.to_period("D").astype(
+            "|S")
+        status_df_pie = status_df_pie.groupby(["status", "date"]).agg({"frequency": "sum"}).reset_index()
 
-    if date == "year":
+        file_type_df_pie = file_type_df[
+            (file_type_df["date"] >= pd.to_datetime(last_30days)) & (file_type_df["date"] <= pd.to_datetime(d_max))]
+        file_type_df_pie["date"] = pd.to_datetime(file_type_df_pie["date"]).dt.tz_localize(None).dt.to_period(
+            "D").astype("|S")
+        file_type_df_pie = file_type_df_pie.groupby(["file_type", "date"]).agg({"frequency": "sum"}).reset_index()
+
+        crawler_df_gran = crawler_df[
+            (crawler_df["date"] >= pd.to_datetime(last_30days)) & (crawler_df["date"] <= pd.to_datetime(d_max))]
+        crawler_df_gran = crawler_df_gran.groupby(["top_directory"]).agg({"frequency": "sum"}).reset_index()
+
+    elif date == "year":
         status_df_pie = status_df[
             (status_df["date"] >= pd.to_datetime(update_last_year)) & (status_df["date"] <= pd.to_datetime(d_max))]
-    print("new_gran")
-    print(status_df_pie)
+        status_df_pie["date"] = pd.to_datetime(status_df_pie["date"]).dt.to_period('M').astype("str")
+        status_df_pie = status_df_pie.groupby(["status", "date"]).agg({"frequency": "sum"}).reset_index()
+
+        file_type_df_pie = file_type_df[
+            (file_type_df["date"] >= pd.to_datetime(update_last_year)) & (
+                    file_type_df["date"] <= pd.to_datetime(d_max))]
+        file_type_df_pie["date"] = pd.to_datetime(file_type_df_pie["date"]).dt.to_period('M').astype("str")
+        file_type_df_pie = file_type_df_pie.groupby(["file_type", "date"]).agg({"frequency": "sum"}).reset_index()
+
+        crawler_df_gran = crawler_df[
+            (crawler_df["date"] >= pd.to_datetime(update_last_year)) & (
+                    crawler_df["date"] <= pd.to_datetime(d_max))]
+        crawler_df_gran = crawler_df_gran.groupby(["top_directory"]).agg({"frequency": "sum"}).reset_index()
+
+    crawler_df_gran.rename(columns={"top_directory": "Top directory", "frequency": "Total Bot Hits"}, inplace=True)
 
     data_sets = {
-        "status_df": status_df_pie.to_json(orient="split", date_format="iso")
+        "status_df": status_df_pie.to_json(orient="split", date_format="iso"),
+        "crawler_df": crawler_df_gran.to_json(orient="split", date_format="iso"),
+        "file_type": file_type_df_pie.to_json(orient="split", date_format="iso")
     }
 
     return json.dumps(data_sets)
@@ -153,7 +239,6 @@ def get_data(bot, date):
 
 @app.callback(
     Output('status_pie_chart', 'figure'),
-    Output('filetype_chart', 'figure'),
     Input('store_data', 'data')
 )
 def update_status_code_pie(needed_data):
@@ -161,30 +246,53 @@ def update_status_code_pie(needed_data):
 
     raw_status_data = json.loads(needed_data)
     ready_status_data = pd.read_json(raw_status_data["status_df"], orient="split")
-    comp_hourly_df = ready_status_data.groupby(["status"]).agg({"count": "sum"}).reset_index()
+    comp_hourly_df = ready_status_data.groupby(["status"]).agg({"frequency": "sum"}).reset_index()
 
-    # fig = px.pie(data_frame=comp_hourly_df, names="status", hole=.5, hover_data="count", col)
+    # fig = px.pie(data_frame=comp_hourly_df, names="status", hole=.5, hover_data="frequency", col)
     # fig.udate_traces()
 
-    fig = go.Figure(data=[go.Pie(labels=comp_hourly_df["status"], values=comp_hourly_df["count"], hole=0.4, )])
+    fig = go.Figure(data=[go.Pie(labels=comp_hourly_df["status"], values=comp_hourly_df["frequency"], hole=0.4, )])
     fig.update_layout(margin=dict(t=50, b=15, l=0, r=0, pad=4), title_text="Status code", height=220)
     fig.update_traces(hoverinfo="label+percent", textinfo="value", textfont_size=10, marker=dict(colors=colors))
 
-    return fig, fig
+    return fig
+
+
+@app.callback(
+    Output('filetype_chart', 'figure'),
+    Input('store_data', 'data')
+)
+def update_file_type_pie(needed_data):
+    colors = ["#23208A", "#BC0D68", "#6494FF", "#6494FF"]
+
+    raw_file_data = json.loads(needed_data)
+    ready_file_data = pd.read_json(raw_file_data["file_type"], orient="split")
+    comp_file_df = ready_file_data.groupby(["file_type"]).agg({"frequency": "sum"}).reset_index()
+    print(comp_file_df)
+
+    # fig = px.pie(data_frame=comp_hourly_df, names="status", hole=.5, hover_data="frequency", col)
+    # fig.udate_traces()
+
+    fig = go.Figure(data=[go.Pie(labels=comp_file_df["file_type"], values=comp_file_df["frequency"], hole=0.4, )])
+    fig.update_layout(margin=dict(t=50, b=15, l=0, r=0, pad=4), title_text="File type", height=220)
+    fig.update_traces(hoverinfo="label+percent", textinfo="value", textfont_size=10, marker=dict(colors=colors))
+
+    return fig
 
 
 @app.callback(
     Output('status_code_bar', 'figure'),
-    Input('store_data', 'data')
+    Input('store_data', 'data'),
 )
 def update_status_code_line(needed_data):
     colors = ["#23208A", "#BC0D68", "#6494FF", "#6494FF"]
     raw_status_data = json.loads(needed_data)
     ready_status_data = pd.read_json(raw_status_data["status_df"], orient="split")
 
-    comp_hourly_df = ready_status_data.groupby(["status", "hour"]).agg({"count": "sum"}).reset_index()
+    print(ready_status_data)
+    fig = px.bar(ready_status_data, x=ready_status_data.columns[1], y="frequency",
+                 color=ready_status_data["status"].astype("str"), barmode="group", )
 
-    fig = px.scatter(comp_hourly_df, x="hour", y="count", color="status")
     fig.update_layout(
         xaxis=dict(
             showline=True,
@@ -194,6 +302,8 @@ def update_status_code_line(needed_data):
             linewidth=2,
             ticks='outside',
             fixedrange=True,
+            type="category",
+            tickformat="%b %d\n%Y",
 
         ),
         yaxis=dict(
@@ -214,10 +324,65 @@ def update_status_code_line(needed_data):
             # Adjust click behavior
             itemclick="toggleothers",
             itemdoubleclick="toggle",
-        )
+        ),
+        # showlegend=True
     )
 
     return fig
+
+
+def filetype_bar_chart(needed_data):
+    colors = ["#23208A", "#BC0D68", "#6494FF", "#6494FF"]
+    raw_status_data = json.loads(needed_data)
+    ready_status_data = pd.read_json(raw_status_data["status_df"], orient="split")
+
+    fig = px.bar(ready_status_data, x=ready_status_data.columns[1], y="frequency",
+                 color=ready_status_data["file_type"].astype("str"), barmode="group", )
+
+    fig.update_layout(
+        xaxis=dict(
+            showline=True,
+            showgrid=False,
+            showticklabels=True,
+            linecolor="rgb(204, 204, 204)",
+            linewidth=2,
+            ticks='outside',
+            fixedrange=True,
+            type="category",
+            tickformat="%b %d\n%Y",
+
+        ),
+        yaxis=dict(
+            # showgrid=False,
+            # zeroline=True,
+            linecolor="rgb(204, 204, 204)",
+            linewidth=2,
+            fixedrange=True,
+            showline=False,
+            showticklabels=True,
+            showgrid=True,
+            gridcolor="#D9D9D9"),
+        plot_bgcolor='white',
+        margin=dict(t=50, b=15, l=0, r=0, pad=4),
+        title_text="File type",
+        height=522,
+        legend=dict(
+            # Adjust click behavior
+            itemclick="toggleothers",
+            itemdoubleclick="toggle",
+        ), )
+
+
+@app.callback(
+    # Output('status_pie_chart', 'figure'),
+    Output('crawler_table', 'data'),
+    Input('store_data', 'data')
+)
+def update_table_crawler(crawler_data):
+    raw_crawler_data = json.loads(crawler_data)
+    ready_crawler_data = pd.read_json(raw_crawler_data["crawler_df"], orient="split")
+
+    return ready_crawler_data.to_dict("records")
 
 
 if __name__ == "__main__":
