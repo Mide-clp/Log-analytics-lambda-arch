6989e580368e12289368ef8cc72ce7bad9644403
Mide clp <aogunnola@gmail.com>
2022-10-01 23:27:47 +0100

    add batch transformation

--- spark-job/batch/batch-job.py
+++ spark-job/batch/batch-job.py
@@ -1,47 +1,46 @@
 from pyspark.sql import SparkSession
 from pyspark.sql import functions as func
 
-spark = SparkSession.builder.master("local[*]").config("dfs.client.use.datanode.hostname", "true").appName(
-    "log-analytics").getOrCreate()
 
-df = spark.read.option("header", "true") \
-    .option("inferSchema", "true") \
-    .load("hdfs://namenode:8020/data/")
-df.count()
-df.show()
+if __name__ == "__main__":
+    spark = SparkSession.builder.master("local[*]").config("dfs.client.use.datanode.hostname", "true").appName(
+        "log-analytics").getOrCreate()
 
+    df = spark.read.option("header", "true") \
+        .option("inferSchema", "true") \
+        .load("hdfs://namenode:8020/data/")
 
+    daily_status = df.select(func.col("status"), func.col("hour"), func.col("month"), func.col("year"),
+                                     func.col("crawler"), func.col("date")) \
+        .groupby("status", "hour", "month", "year", "date", "crawler").agg(func.count("status").alias("frequency"))
 
-import datetime
+    file_type_data = df.withColumn("file_type", func.regexp_extract("endpoint",
+                                                                            r"\.(css|jpg|PHP|html|png|gif|jpeg|json|js)",
+                                                                            1)).where(func.col("file_type") != "")
 
-d = datetime.datetime.
+    file_type_daily = file_type_data.select(func.col("file_type"), func.col("crawler"), func.col("hour"),
+                                            func.col("month"), func.col("year"), func.col("date")).groupBy("file_type",
+                                                                                                           "hour",
+                                                                                                           "month",
+                                                                                                           "year",
+                                                                                                           "date",
+                                                                                                           "crawler").agg(
+        func.count("file_type").alias("frequency"))
 
-def ArrayChallenge(arr):
-    buildings = len(arr)
-    l_buildings = []
-    r_buildings = []
+    page_crawler = df.withColumn("top_directory", func.regexp_extract("endpoint", "(\/m\/\w+|\/\w+)", 1))
 
-    water = 0
+    pages_crawled = page_crawler.select("hour", "month", "year", "date", "crawler", "top_directory").groupBy("hour",
+                                                                                                             "month",
+                                                                                                             "year",
+                                                                                                             "crawler",
+                                                                                                             "date",
+                                                                                                             "top_directory").agg(
+        func.count("top_directory").alias("frequency"))
 
-    current_max_building = 0
-    for x in range(buildings):
-        if arr[x] > current_max_building:
-            current_max_building = arr[x]
+    pages_crawled = pages_crawled.where(func.col("top_directory") != "")
 
-        l_buildings.append(current_max_building)
-    new_max_building = 0
+    bot_hit = df.select("hour", "month", "year", "date", "crawler").groupBy("hour", "month", "year",
+                                                                                    "crawler",
+                                                                                    "date").agg(func.count("hour")
+                                                                                                .alias("frequency"))
 
-    for x in range(buildings + 1):
-        if arr[-x] > new_max_building:
-            new_max_building = arr[-x]
-        r_buildings.insert(0, current_max_building)
-
-    for i, building in enumerate(arr):
-        water += min(l_buildings[i], r_buildings[i]) - building
-
-    return water
-
-
-print(ArrayChallenge(input()))
-
-# https://stackoverflow.com/questions/24414700/water-collected-between-towers?rq=1
