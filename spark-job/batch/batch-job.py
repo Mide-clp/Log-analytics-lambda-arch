ac79bf72cfb060fd08277e57aef5b21cb83350a4
Mide clp <aogunnola@gmail.com>
2022-10-07 12:24:25 +0100

    complete batch and stream pipeline

--- spark-job/batch/batch-job.py
+++ spark-job/batch/batch-job.py
@@ -1,14 +1,15 @@
 from pyspark.sql import SparkSession
 from pyspark.sql import functions as func
-
+from batch_write import write_to_cassandra_bot_hit, write_to_cassandra_crawler, write_to_cassandra_file_type, write_to_cassandra_status_code
 if __name__ == "__main__":
     # spark = SparkSession.builder.master("spark://spark-master:7077").config("dfs.client.use.datanode.hostname", "true").appName(
     #     "log-analytics").getOrCreate()
 
-    spark = SparkSession.builder.master("local[*]").appName(
+    spark = SparkSession.builder.master("local[*]").config("dfs.client.use.datanode.hostname", "true").appName(
         "log-analytics").getOrCreate()
 
     df = spark.read.option("header", "true") \
+        .format("parquet") \
         .option("inferSchema", "true") \
         .load("hdfs://namenode:8020/data/log_data/") # hdfs://namenode:8020/data/ ./data_log/
 
@@ -52,12 +53,16 @@ if __name__ == "__main__":
                                                                                         .alias("frequency"))
 
     bot_hit = bot_hit.withColumn("current_timestamp", func.current_timestamp())
+    # bot_hit.agg(func.sum("frequency")).show()
+    # pages_crawled.agg(func.sum("frequency")).show()
+    # daily_status.agg(func.sum("frequency")).show()
+    # file_type_daily.agg(func.sum("frequency")).show()
 
-    bot_hit.printSchema()
-    pages_crawled.show()
-    bot_hit.select("*").agg(func.sum("frequency")).show()
-    # bot_hit.write.saveAsTable()
+    write_to_cassandra_bot_hit(bot_hit)
+    write_to_cassandra_crawler(pages_crawled)
+    write_to_cassandra_status_code(daily_status)
+    write_to_cassandra_file_type(file_type_daily)
 
-# docker exec spark-master /spark/bin/spark-submit  --master spark://localhost:7077 opt/spark_store/batch/batch-job.py
+# docker exec spark-master /spark/bin/spark-submit  --packages com.datastax.spark:spark-cassandra-connector_2.12:3.0.0 --master spark://localhost:7077 opt/spark_store/batch/batch-job.py
 
 # spark-submit  spark-job/batch/batch-job.py
--- spark-job/streaming/stream_write.py
+++ spark-job/streaming/stream_write.py
@@ -3,7 +3,7 @@
 def write_to_cassandra_status_code(_status_df, _batch_id):
     _status_df.write \
         .format("org.apache.spark.sql.cassandra") \
-        .option("spark.cassandra.connection.host", "127.0.0.1") \
+        .option("spark.cassandra.connection.host", "cassandra") \
         .option("spark.cassandra.connection.port", "9042") \
         .option("keyspace", "log_analytics") \
         .option("table", "status_code_realtime") \
@@ -14,7 +14,7 @@ def write_to_cassandra_status_code(_status_df, _batch_id):
 def write_to_cassandra_file_type(_file_type_df, _batch_id):
     _file_type_df.write \
         .format("org.apache.spark.sql.cassandra") \
-        .option("spark.cassandra.connection.host", "127.0.0.1") \
+        .option("spark.cassandra.connection.host", "cassandra") \
         .option("spark.cassandra.connection.port", "9042") \
         .option("keyspace", "log_analytics") \
         .option("table", "file_type_realtime") \
@@ -25,7 +25,7 @@ def write_to_cassandra_file_type(_file_type_df, _batch_id):
 def write_to_cassandra_crawler(_crawler_df, _batch_id):
     _crawler_df.write \
         .format("org.apache.spark.sql.cassandra") \
-        .option("spark.cassandra.connection.host", "127.0.0.1") \
+        .option("spark.cassandra.connection.host", "cassandra") \
         .option("spark.cassandra.connection.port", "9042") \
         .option("keyspace", "log_analytics") \
         .option("table", "crawler_frequency_realtime") \
@@ -36,7 +36,7 @@ def write_to_cassandra_crawler(_crawler_df, _batch_id):
 def write_to_cassandra_bot_hit(_bot_hit_df, _batch_id):
     _bot_hit_df.write \
         .format("org.apache.spark.sql.cassandra") \
-        .option("spark.cassandra.connection.host", "127.0.0.1") \
+        .option("spark.cassandra.connection.host", "cassandra") \
         .option("spark.cassandra.connection.port", "9042") \
         .option("keyspace", "log_analytics") \
         .option("table", "bot_hits_realtime") \
